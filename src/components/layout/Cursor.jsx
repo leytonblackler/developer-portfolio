30b38bb8cf7593dc23214fe154a066c49336d0d1
Leyton Blackler <leytonblackler@gmail.com>
2020-07-13 14:28:13 +1200

    Added cursor listeners for leave and enter

--- src/components/layout/Cursor.jsx
+++ src/components/layout/Cursor.jsx
@@ -4,30 +4,39 @@ import { motion } from "framer-motion";
 
 const Circle = styled(motion.div)`
   border-radius: 1000px;
-  top: var(--y);
-  left: var(--x);
+  top: 0;
+  left: 0;
   position: absolute;
   z-index: 200;
   border-color: #ffffff;
   border-style: solid;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%) translate3d(var(--x), var(--y), 0);
   mix-blend-mode: difference;
   pointer-events: none;
 `;
 
 const Cursor = ({ sectionIndex }) => {
   const [mouseDown, setMouseDown] = useState(false);
+  const [mouseInWindow, setMouseInWindow] = useState(false);
 
   useEffect(() => {
     // Add mouse down and mouse up listeners to set whether the cursor is being pressed.
     document.onmousedown = () => setMouseDown(true);
     document.onmouseup = () => setMouseDown(false);
+
+    // Add mouse leave and enter listeners to set whether the cursor is in the window.
+    document.onmouseleave = () => setMouseInWindow(false);
+    document.onmouseenter = () => setMouseInWindow(true);
+
     // Add a mouse move listener to set the cursor position.
     document.onmousemove = (event) => {
+      if (!mouseInWindow) {
+        setMouseInWindow(true);
+      }
       document.body.style.setProperty("--x", `${event.clientX}px`);
       document.body.style.setProperty("--y", `${event.clientY}px`);
     };
-  }, []);
+  }, [mouseInWindow]);
 
   const currentStyle = {
     width: mouseDown ? 400 : 15,
@@ -41,6 +50,12 @@ const Cursor = ({ sectionIndex }) => {
   if (sectionIndex > 0) {
     currentStyle.borderWidth = 0;
     currentStyle.backgroundColor = "rgba(255, 255, 255, 1)";
+  }
+
+  if (!mouseInWindow) {
+    currentStyle.borderWidth = 0;
+    currentStyle.width = 0;
+    currentStyle.height = 0;
   }
 
   return (
@@ -60,10 +75,10 @@ const Cursor = ({ sectionIndex }) => {
         },
       }}
       initial={{
-        width: 15,
-        height: 15,
+        width: 0,
+        height: 0,
         backgroundColor: "rgba(255, 255, 255, 0)",
-        borderWidth: 7,
+        borderWidth: 0,
       }}
       animate={currentStyle}
     />
