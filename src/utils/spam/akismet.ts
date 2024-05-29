e5aef07b47b764ea96ac880c5c10da020a173886
Leyton Blackler <18431950+leytonblackler@users.noreply.github.com>
2024-05-30 10:57:54 +1200

    Rename SITE_URL env var to BASE_URL

--- src/utils/spam/akismet.ts
+++ src/utils/spam/akismet.ts
@@ -1,20 +1,18 @@
 import { AkismetClient } from "akismet-api";
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, str, url } from "envalid";
 
 /**
  * Parse the environment variables for Akismet API configuration.
  */
-const { AKISMET_API_KEY, SITE_URL } = cleanEnv(process.env, {
+const { AKISMET_API_KEY, BASE_URL } = cleanEnv(process.env, {
   AKISMET_API_KEY: str(),
-  SITE_URL: str(),
+  BASE_URL: url(),
 });
-
-console.log("SITE_URL", SITE_URL);
 
 /**
  * Initialise the Akismet API client.
  */
 export const akismet = new AkismetClient({
   key: AKISMET_API_KEY,
-  blog: SITE_URL,
+  blog: BASE_URL,
 });
