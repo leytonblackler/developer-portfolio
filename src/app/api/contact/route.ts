316a889e8727deda0d4ce89302d206b088766b9e
Leyton Blackler <hello@leytonblackler.dev>
2025-04-29 12:58:46 +1000

    Disable edge function runtime for contact API route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -297,3 +297,11 @@ export const POST: ContactFormSubmissionEndpoint = async (request) => {
   console.info(chalk.green("Message processed successfully!\n"));
   return NextResponse.json(null, { status: StatusCodes.OK });
 };
+
+export const config = {
+  /**
+   * Ensures it’s NOT deployed as an Edge Function, as this can cause issues
+   * with chalk.
+   */
+  runtime: "nodejs",
+};
