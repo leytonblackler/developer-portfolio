31bd5dfb24861f1d2cf9fe7bc3b4febc0686d141
Leyton Blackler <hello@leytonblackler.dev>
2025-05-11 14:36:50 +1000

    Update remote patterns for new Hygraph CDN

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
        */
       {
         protocol: "https",
+        hostname: "**.graphassets.com",
+      },
+      {
+        protocol: "https",
         hostname: "media.graphassets.com",
       },
       /**
